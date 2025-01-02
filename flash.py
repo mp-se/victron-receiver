db6da3ce7dbebd426353f6594adf2cd4aaa3f4f8
Magnus <mp-se@noreply.github.com>
2025-01-02 17:21:31 +0100

    Update flash.py

--- flash.py
+++ flash.py
@@ -14,9 +14,9 @@ flash = ""
 port = ' --port "' + sys.argv[2] + '"'
 
 if sys.argv[1] == "esp32-tft":
-  flash = " --chip esp32 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size 4MB 0x1000 bin/bootloader32.bin 0x8000 bin/partitions32.bin 0xe000 bin/boot32.bin 0x10000 bin/firmware32_tft.bin"
+  flash = " --chip esp32 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size 4MB 0x1000 bin/bootloader32pro.bin 0x8000 bin/partitions32.bin 0xe000 bin/boot32pro.bin 0x10000 bin/firmware32_tft.bin"
 elif sys.argv[1] == "esp32s3-tft":
-  flash = " --chip esp32s3 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 16MB 0x0000 bin/bootloader32s3.bin 0x8000 bin/partitions32s3.bin 0xe000 bin/boot32s3.bin 0x10000 bin/firmware32s3_tft.bin"
+  flash = " --chip esp32s3 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 16MB 0x0000 bin/bootloader32s3pro.bin 0x8000 bin/partitions32s3.bin 0xe000 bin/boot32s3pro.bin 0x10000 bin/firmware32s3_tft.bin"
 elif sys.argv[1] == "esp32s3w-tft":
   flash = " --chip esp32s3 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 16MB 0x0000 bin/bootloader32s3w.bin 0x8000 bin/partitions32s3w.bin 0xe000 bin/boot32s3w.bin 0x10000 bin/firmware32s3w_tft.bin"
 elif sys.argv[1] == "esp32c3":
